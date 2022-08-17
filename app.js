4d3dc1d7b6c41d9d292792dc999469396c623436
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2022-08-17 13:50:00 -0400

    push

--- app.js
+++ app.js
@@ -4,16 +4,14 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('In the middleware!');
-  next(); // allow the request to continue to the next middleware
+app.use('/add-product', (req, res, next) => {
+  console.log('In another middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
 });
 
-app.use((req, res, next) => {
+app.use('/', (req, res, next) => {
   console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>'); // send a response
 });
 
-const server = http.createServer(app);
-
-server.listen(3010);
+app.listen(3010); // cleaner style
