2ebd968807cae2f5fc1ed689b99e7d5682c42fa7
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2022-08-18 13:49:00 -0400

    push

--- app.js
+++ app.js
@@ -3,20 +3,12 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// registers a middleware
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extneded: false}));
 
-app.use('/add-product', (req, res, next) => {
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></input></form>');
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>'); // send a response
-});
-
-app.listen(3010); // cleaner style
+app.listen(3010);
