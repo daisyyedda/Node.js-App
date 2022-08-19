d9e07de6edda99cc54c429cc0227739fe8f37b77
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2022-08-19 12:58:56 -0400

    push

---
+++ controllers/shop.js
@@ -0,0 +1,33 @@
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products',
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+    });
+  }); 
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    prods: products,
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('/hop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
