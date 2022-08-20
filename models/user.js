fad766c0339047751ee33bfb8a1edb1c1377c56d
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2022-08-20 17:10:00 -0400

    added user account

--- models/user.js
+++ models/user.js
@@ -1,1 +1,16 @@
-const 
+const Sequelize = require('sequelize');
+
+const sequelize = require('../util/database');
+
+const User = sequelize.define('user', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  },
+  name: Sequelize.STRING,
+  email: Sequelize.STRING
+});
+
+module.exports = User;
