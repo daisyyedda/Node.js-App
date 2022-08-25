88173f7288ea3cecc399ea4162ef0848d92ef9f2
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2022-08-25 17:55:10 -0400

    adding to cart functionality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findByPk('6307e8b4f2dec3f3630b8bde')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
