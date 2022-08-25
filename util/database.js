68745ca19c538bc731e2c5343f5071723b4c393a
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2022-08-25 16:13:30 -0400

    finishing up the connection

--- util/database.js
+++ util/database.js
@@ -1,18 +1,29 @@
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 
+let _db;
+
 const mongoConnect = (callback) => {
   MongoClient.connect(
-    'mongodb+srv://daisyyedda:Yc-20021116@cluster0.2mzvjvg.mongodb.net/?retryWrites=true&w=majority'
+    'mongodb+srv://daisyyedda:Yc-20021116@cluster0.2mzvjvg.mongodb.net/shop?retryWrites=true&w=majority'
     )
     .then(client => {
       console.log('Connected!');
-      callback(client);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err);
+      throw err;
     });  
 };
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
 
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
