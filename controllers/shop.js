b2ed1478fa35ed02f8bdbbd4d91739e71dcb833d
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2022-08-26 10:34:54 -0400

    add to cart/edit

--- controllers/shop.js
+++ controllers/shop.js
@@ -54,17 +54,12 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   req.user
     .getCart()
-    .then(cart => {
-      return cart
-        .getProducts()
-        .then(products => {
-          res.render('shop/cart', {
-            path: '/cart',
-            pageTitle: 'Your Cart',
-            products: products
-          });
-        })
-        .catch(err => console.log(err));
+    .then(products => {
+      res.render('shop/cart', {
+        path: '/cart',
+        pageTitle: 'Your Cart',
+        products: products
+      });
     })
     .catch(err => console.log(err));
 };
@@ -73,10 +68,11 @@ exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
   Product.findByPk(prodId)
     .then(product => {
-      return req.user.addToCart(product);
+      return req.user.addToCart(product);  
   })
     .then(result => {
       console.log(result);
+      res.redirect('/cart');
   });
   // let fetchedCart;
   // let newQuantity = 1;
