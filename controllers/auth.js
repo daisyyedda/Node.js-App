e587f6bd04b8fcde1f1778042561a6d174e52b25
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2022-08-29 12:32:44 -0400

    added email and password validations

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,7 @@ const crypto = require('crypto');
 
 const bcrypt = require('bcryptjs');
 var nodemailer = require('nodemailer');
+const { validationResult } = require('express-validator');
 
 var transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -78,43 +79,45 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
-  User.findOne({email: email})
-    .then(userDoc => {
-      if (userDoc) {
-        req.flash('error', 'E-Mail exists already, please pick a different one.');
-        return res.redirect('/signup');
-      }
-      return bcrypt
-        .hash(password, 12)
-        .then(hashedPassword => {
-          const user = new User({
-            email: email,
-            password: hashedPassword,
-            cart: { items: [] }
-          });
-          return user.save();
-        })
-        .then(result => {
-          res.redirect('/login');
-          var mailOptions = {
-            from: 'daisyyedda@gmail.com',
-            to: email,
-            subject: 'Signup succeeded!',
-            html: '<h1>You successfully signed up!</h1>'
-          };
-          return transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-              console.log(error);
-            }
-          }); 
-        })
-        .catch(err => {
-          console.log(err);
-        });
-    })
-    .catch(err => {
-      console.log(err);
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(422)
+    .render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
     });
+  }
+  bcrypt
+  .hash(password, 12)
+  .then(hashedPassword => {
+    const user = new User({
+      email: email,
+      password: hashedPassword,
+      cart: { items: [] }
+    });
+    return user.save();
+  })
+  .then(result => {
+    res.redirect('/login');
+    var mailOptions = {
+      from: 'daisyyedda@gmail.com',
+      to: email,
+      subject: 'Signup succeeded!',
+      html: '<h1>You successfully signed up!</h1>'
+    };
+    return transporter.sendMail(mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      }
+    }); 
+  })
+  .catch(err => {
+    console.log(err);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
 
 exports.postLogout = (req, res, next) => {
