b7490e4abd35e9238e0c95e71cb68f5778ef87e8
daisyyedda <65566095+daisyyedda@users.noreply.github.com>
2022-09-03 16:43:33 -0400

    async requests

--- public/css/js/admin.js
+++ public/css/js/admin.js
@@ -1,4 +1,20 @@
-const deleteProduct = (btn) => {
+const deleteProduct = btn => {
   const prodId = btn.parentNode.querySelector('[name=productId]').value;
   const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+
+  fetch('/admin/product/' + prodId, {
+    method: 'DELETE',
+    headers: {
+      'csrf-token': csrf
+    }
+  })
+  .then(result => {
+    return result.json();
+  })
+  .then(data => {
+    console.log(data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 };
